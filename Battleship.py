41a4da40e9a8590f20c9ba5c0dd9d370841ce5a7
Dan Faro <danieln.faro@gmail.com>
2017-02-23 20:54:35 -0500

    added variables for boardsize and turn

--- Battleship.py
+++ Battleship.py
@@ -1,9 +1,9 @@
 from random import randint
 
 board = []
-
-for x in range(5):
-    board.append(["O"] * 5)
+boardsize = 10
+for x in range(boardsize):
+    board.append(["O"] * boardsize)
 
 def print_board(board):
     for row in board:
@@ -23,23 +23,25 @@ ship_col = random_col(board)
 #print ship_row
 #print ship_col
 
-for turn in range(4):
+turns = 10
+
+for turn in range(turns):
     print "Turn", turn + 1
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
+    guess_row = int(raw_input("Guess Row:")) - 1
+    guess_col = int(raw_input("Guess Col:")) - 1
 
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
         break
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0          or guess_col > 4):
+        if (guess_row < 0 or guess_row > boardsize-1) or (guess_col < 0 or guess_col > boardsize-1):
             print "Oops, that's not even in the ocean."
         elif board[guess_row][guess_col] == "X":
             print "You guessed that one already."
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
-    if turn == 3:
+    if turn == turns-1:
         print "Game Over"
     # Print (turn + 1) here!
     print_board(board)
